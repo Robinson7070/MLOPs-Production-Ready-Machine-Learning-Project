c8f688d88263b0a112b58a0d00e3c1f89ca69cb4
Robinson7070 <robinsonvictorchukwudi@gmail.com>
2025-02-05 12:50:07 +0100

    artifact updated

--- us_visa/entity/artifact_entity.py
+++ us_visa/entity/artifact_entity.py
@@ -3,8 +3,8 @@ from dataclasses import dataclass
 
 @dataclass
 class DataIngestionArtifact:
-    trained_file_path:str
-    test_file_path:str
+    trained_file_path:str 
+    test_file_path:str 
 
 
 
@@ -35,3 +35,21 @@ class ClassificationMetricArtifact:
 class ModelTrainerArtifact:
     trained_model_file_path:str 
     metric_artifact:ClassificationMetricArtifact
+
+
+
+@dataclass
+class ModelEvaluationArtifact:
+    is_model_accepted:bool
+    changed_accuracy:float
+    s3_model_path:str 
+    trained_model_path:str
+
+
+
+@dataclass
+class ModelPusherArtifact:
+    bucket_name:str
+    s3_model_path:str
+
+
